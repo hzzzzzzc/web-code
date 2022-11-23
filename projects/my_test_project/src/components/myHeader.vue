111b834f6f7f59bdb0213ee21121bed27fb27c82
hzzzzzzc <1292153062@qq.com>
2022-11-23 18:26:06 +0800

    practice

---
+++ projects/my_test_project/src/components/myHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="header">
+    <div class="header-name">
+      {{name}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      name: "myTestProject",
+    }
+  }
+}
+</script>
+
+<style scoped>
+ .header{
+  width: 100%;
+  height: 20vh;
+  background-color: rgb(214, 89, 89);
+  /* position: absolute;
+  top: 0; */
+ }
+</style>
